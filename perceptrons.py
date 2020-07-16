a6782e4757a54fb98e7bef90c7515f0edfdb3a78
adnanmurtaza1 <adnanmurtaza@gmail.com>
2020-07-16 11:33:27 -0400

    after tutoring w jason

--- perceptrons.py
+++ perceptrons.py
@@ -58,6 +58,73 @@ def check(n, w, b):
 
     return float(numerator/denominator)
 
+'''
+n = 9
+w = (1, 1)
+bit = len(w)
+b = -1.5
+pretty_print_tt(truth_table(bit, n))
+print(check(n, w, b))
+'''
+
+def step(num):
+    if num > 0:
+        return 1
+    return 0
+
+def truth_table(bits, n):
+    size = 2**bits
+    answers = fix_conversion(size, n)
+    result = ()
+    current = size - 1
+    while current > -1:
+        result = result + ((fix_conversion(bits, current), answers[size - current - 1]), )
+        current = current - 1
+
+    return result
+
+def fix_conversion(bit, num):
+    tup = ()
+    tup = dec_to_binary(num, tup)
+    while len(tup) < bit:
+        tup = (0,) + tup
+    return tup
+
+def dec_to_binary(num, tup):
+    if num > 1:
+       tup = dec_to_binary(num//2, tup)
+
+    tup = tup + (num%2, )
+    return tup
+
+def pretty_print_tt(table):
+    if len(table) != 0:
+        for x in range(0, len(table)):
+            for y in range(0, len(table[0][0])):
+                print(table[x][0][y], " ", end="")
+            print("|", table[x][1], end= "")
+            print()
+
+def perceptron(A, w, b, x):
+    return A(dot_product(w, x) + b)
+
+def dot_product(vec1, vec2):
+    sum = 0
+    for x in range(0, len(vec1)):
+        sum = sum + vec1[x]*vec2[x]
+
+    return sum
+
+def check(n, w, b):
+    temp_table = truth_table(len(w), n)
+    denominator = float(len(temp_table))
+    numerator = 0.0
+    for x in range(len(temp_table)):
+        if perceptron(step, w, b, temp_table[x][0]) == temp_table[x][1]:
+            numerator = numerator + 1
+
+    return float(numerator/denominator)
+
 def sum_vectors(vec1, vec2):
     sum = ()
     for index in range(len(vec1)):
@@ -110,7 +177,8 @@ def table_work(bit, num, og_og_weight, og_og_bias, change_frac):
                     pretty_print_tt(table)
                     print(og_weight, og_bias)
                     return works
-                    break #breaks out of the epoch loop
+    pretty_print_tt(table)
+    print(og_weight, og_bias)
     return False
 
 
@@ -130,4 +198,10 @@ for table_num in range(2**(2**bit)):
 print(str(working) + " of the possible " + str(2**(2**bit)) + " tables work")
 '''
 
-print(table_work(2, 8, (0, 0), 0, 1))
+
+# #print(table_work(2, 8, (0, 0), 0, 1))
+# input_vector = (0, 0)
+# p3_result = perceptron(step, (-1, -2), 3, input_vector)
+# p4_result = perceptron(step, (1, 1), 0, input_vector)
+# p5_result = perceptron(step, (1, 2), -2, (p3_result, p4_result))
+# print(p5_result)
